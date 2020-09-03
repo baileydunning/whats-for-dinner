ee6b43f34451b79e6adb0ad6a50461aad445b277
Bailey Dunning <bwdunning@gmail.com>
2020-09-03 17:56:05 -0600

    Make text appear on button click

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ var sideDish = ["Coleslaw", "Garlic Butter Mushrooms"]
 var mainDish = ["Spaghetti & Meatballs", "Thai Yellow Curry"]
 var dessert = ["Apple Pie", "Black Forest Cake"]
 
-var letsCookButton = document.querySelector('.lets-cook-button')
 var cookpot = document.querySelector('.cookpot')
+var suggestion = document.querySelector('.suggestion')
+var letsCookButton = document.querySelector('.lets-cook-button')
 
 letsCookButton.addEventListener('click', showDish)
 
 function showDish() {
   event.preventDefault()
   cookpot.classList.add('hidden')
+  suggestion.classList.remove('hidden')
 }
