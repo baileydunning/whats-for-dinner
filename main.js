f795b4dc92f48683b3b4e339e7a0848338d9dff5
Bailey Dunning <bwdunning@gmail.com>
2020-09-03 17:31:16 -0600

    Hide cookpot img upon click of letsCookButton

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ var mainDish = ["Spaghetti & Meatballs", "Thai Yellow Curry"]
 var dessert = ["Apple Pie", "Black Forest Cake"]
 
 var letsCookButton = document.querySelector('.lets-cook-button')
+var cookpot = document.querySelector('.cookpot')
 
 letsCookButton.addEventListener('click', showDish)
+
+function showDish() {
+  event.preventDefault()
+  cookpot.classList.add('hidden')
+}
