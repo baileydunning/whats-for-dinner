426b7cf37c000da112c18f0cade97cf079298fc0
Bailey Dunning <bwdunning@gmail.com>
2020-09-04 10:59:48 -0600

    Add functionality to form to associate input choice with output

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var sideDishes = ['Coleslaw', 'Garlic Butter Mushrooms', 'Spanish Rice', 'Chips & Salsa', 'Cheese & Crackers', 'Mashed Potatoes']
+var sideDishes = ['Coleslaw', 'Garlic Butter Mushrooms', 'Spanish Rice', 'Chips & Salsa', 'Cheese & Crackers', 'Mashed Potatoes', 'Hummus']
 var mainDishes = ['Spaghetti & Meatballs', 'Mac & Cheese', 'Dumplings', 'Garden Salad', 'BLT', 'Chili', 'Fish Tacos', 'Thai Yellow Curry', 'Sesame Chicken', 'Turkey Burgers', 'Fettucini Alfredo']
-var desserts = ['Apple Pie', 'Black Forest Cake', 'Chocolate Chip Cookies', 'Lemon Bars', 'Red Velvet Cupcakes']
+var desserts = ['Apple Pie', 'Black Forest Cake', 'Chocolate Chip Cookies', 'Lemon Bars', 'Red Velvet Cupcakes', 'Oatmeal Raisin Cookies']
 
 var cookpot = document.querySelector('.cookpot')
 var suggestionBox = document.querySelector('.suggestion-box')
 var returnDishSuggestion = document.querySelector('.dish-suggestion')
 var letsCookButton = document.querySelector('.lets-cook-button')
+var choice = document.querySelectorAll('.dish-type')
 
 letsCookButton.addEventListener('click', showDish)
 
@@ -14,14 +15,36 @@ function getRandomIndex(array) {
   return array[randomIndex]
 }
 
-function showDish() {
-  event.preventDefault()
+function cookingTime() {
   cookpot.classList.add('hidden')
   suggestionBox.classList.remove('hidden')
-  var sideDish = getRandomIndex(sideDishes);
+}
+
+function showDish() {
+  event.preventDefault()
+  cookingTime()
+  if (document.getElementById('sideDish').checked === true) {
+    showSideDish()
+  }
+  if (document.getElementById('mainDish').checked === true) {
+    showMainDish()
+  }
+  if (document.getElementById('dessert').checked === true) {
+    showDessert()
+  }
+}
+
+function showSideDish() {
+  var sideDish = getRandomIndex(sideDishes)
   returnDishSuggestion.innerText = `${sideDish}`
 }
 
-// function showSideDish(sideDishes) {
-//   var sideDish = getRandomIndex(sideDishes);
-// }
+function showMainDish() {
+  var mainDish = getRandomIndex(mainDishes)
+  returnDishSuggestion.innerText = `${mainDish}`
+}
+
+function showDessert() {
+  var dessert = getRandomIndex(desserts)
+  returnDishSuggestion.innerText = `${dessert}`
+}
