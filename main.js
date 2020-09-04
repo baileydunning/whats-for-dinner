87fb72900335e4a34818b45cf0afa30bd9e94ea4
Bailey Dunning <bwdunning@gmail.com>
2020-09-03 19:26:57 -0600

    Change innerText on button click

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var sideDish = ["Coleslaw", "Garlic Butter Mushrooms"]
-var mainDish = ["Spaghetti & Meatballs", "Thai Yellow Curry"]
-var dessert = ["Apple Pie", "Black Forest Cake"]
+var sideDish = ['Coleslaw', 'Garlic Butter Mushrooms']
+var mainDish = ['Spaghetti & Meatballs', 'Thai Yellow Curry']
+var dessert = ['Apple Pie', 'Black Forest Cake']
 
 var cookpot = document.querySelector('.cookpot')
-var suggestion = document.querySelector('.suggestion')
+var suggestionBox = document.querySelector('.suggestion-box')
+var returnDishSuggestion = document.querySelector('.dish-suggestion')
 var letsCookButton = document.querySelector('.lets-cook-button')
 
 letsCookButton.addEventListener('click', showDish)
@@ -11,5 +12,11 @@ letsCookButton.addEventListener('click', showDish)
 function showDish() {
   event.preventDefault()
   cookpot.classList.add('hidden')
-  suggestion.classList.remove('hidden')
+  suggestionBox.classList.remove('hidden')
+  returnDishSuggestion.innerText = "DELICIOUSNESS"
 }
+
+// function getRandomIndex(array) {
+//   var randomIndex = Math.floor(Math.random() * array.length);
+//   return array[randomIndex];
+// }
